c8890efff28564e02a40c27fba0e4b06a9f31426
Arnas Bradauskas <arnas1373@gmail.com>
2023-06-28 23:20:44 +0300

    Fixed result format

--- Models/Result.cs
+++ Models/Result.cs
@@ -2,11 +2,13 @@ using Newtonsoft.Json;
 
 public class Result
 {
+    [JsonProperty("region")]
     public string Region { get; }
 
     [JsonIgnore]
     public List<Location> MatchedLocations { get; } = new List<Location>();
 
+    [JsonProperty("matched_locations")]
     public List<string> MatchedLocationNames 
     { 
         get { return MatchedLocations.Select(loc => loc.Name).ToList(); } 
